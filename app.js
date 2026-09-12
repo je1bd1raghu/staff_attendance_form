2e13d478784eee5593e6402570d49081c6fb7d21
je1bd1raghu <je1_bd1_raghu@wbphed.gov.in>
2026-09-13 04:26:59 +0530

    redesigned pages

--- app.js
+++ app.js
@@ -251,7 +251,7 @@ function updateClock() {
   document.getElementById('dispDate').textContent = bd(now.getDate()) + ' ' + MONTH_ABBR_BN[now.getMonth()] + ' ' + bd(now.getFullYear());
   document.getElementById('dispTime').textContent = bd(pad(now.getHours())) + ':' + bd(pad(now.getMinutes())) + ':' + bd(pad(now.getSeconds()));
   document.getElementById('dispDay').textContent  = DAY_ABBR_BN[now.getDay()];
-  document.getElementById('headerDate').textContent = DAY_FULL_BN[now.getDay()] + ', ' + bd(now.getDate()) + ' ' + MONTH_ABBR_BN[now.getMonth()] + ', ' + bd(now.getFullYear());
+  document.getElementById('headerDateText').textContent = DAY_FULL_BN[now.getDay()] + ', ' + bd(now.getDate()) + ' ' + MONTH_ABBR_BN[now.getMonth()] + ', ' + bd(now.getFullYear());
 }
 function pad(n) { return n < 10 ? '0' + n : '' + n; }
 
